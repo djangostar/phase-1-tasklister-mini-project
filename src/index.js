0f87f65b85f440f6594a1e53053ddfa909ba50c8
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-27 09:27:26 -0800

    Needs More Functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const newTaskForm = document.getElementById('create-task-form')
+  newTaskForm.addEventListener('submit', createNewTask)
+})
+
+function createNewTask(e) {
+  e.preventDefault()
+
+  const newTaskDescription = document.getElementById('new-task-description')
+  const newTask = document.createElement('li')
+  newTask.innerText = newTaskDescription.value
+
+  appendNewTask(newTask)
+  e.target.reset()
+}
+
+function appendNewTask(newTask) {
+  const toDoTask = document.getElementById('tasks')
+  toDoTask.append(newTask)
+}
